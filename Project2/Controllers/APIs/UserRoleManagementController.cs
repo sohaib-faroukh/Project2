e267b6f7762901a22615ccf94e3a31b2872b3571
SOHAIB <SOHAIB@SOHAI-PC>
2019-03-11 20:52:52 +0200

    Adding user role management cotroller

---
+++ Project2/Controllers/APIs/UserRoleManagementController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Project2.Controllers.APIs
+{
+    public class UserRoleManagementController : ApiController
+    {
+        // GET: api/UserRoleManagement
+        [HttpGet]
+        public IHttpActionResult getUsers()
+        {
+            return Json(new {  });
+        }
+    }
+}
