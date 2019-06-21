c14eb089c5c7220ecd524ef51a63f521f60d84bf
sohaib-faroukh <so.872562@gmail.com>
2019-06-22 02:53:30 +0300

    UI creating survey management Module and update other files

--- CustomerProfileBank.UI/src/app/app.routing.ts
+++ CustomerProfileBank.UI/src/app/app.routing.ts
@@ -5,8 +5,6 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
-import { UsersComponent } from './User Role Management/users/users.component';
-
 const routes: Routes = [
   {
     path: '',
