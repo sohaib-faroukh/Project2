c13eb3808f81896bbd286f8311fa5c793fddc304
SOHAIBF <SOHAIBF@DESKTOP-U8DICOS>
2019-06-27 02:11:32 +0300

    Finish customer APIs first release

--- CustomerProfileBank.Models/Repositories/QuestionRepo.cs
+++ CustomerProfileBank.Models/Repositories/QuestionRepo.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CustomerProfileBank.Models.Context;
+using CustomerProfileBank.Models.Repositories.Generic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace CustomerProfileBank.Models.Repositories
 {
-    class QuestionRepo
+    public class QuestionRepo : GeneralRepository<DataBaseContext, QuestionRepo>
     {
     }
 }
